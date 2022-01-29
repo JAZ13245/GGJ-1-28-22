1218a24f3dab4a46228da5413b27a008559d7d1a
Gamertag <cre3363@g.rit.edu>
2022-01-29 03:44:35 -0500

    Both Players Move :D

--- GGJ-1-28-22/Assets/Scripts/GameManager.cs
+++ GGJ-1-28-22/Assets/Scripts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-
+    public GameObject playerPrefab;
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1;
+        var player1 = PlayerInput.Instantiate(playerPrefab, controlScheme: "PlayerOne", pairWithDevice: Keyboard.current);
+        var player2 = PlayerInput.Instantiate(playerPrefab, controlScheme: "PlayerTwo", pairWithDevice: Keyboard.current);
     }
 
     // Update is called once per frame
