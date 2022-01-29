47fd979b0fe4578b1d963bec51f4e5483a0879af
Adrotic <adroticofficial@gmail.com>
2022-01-28 20:22:52 -0500

    line moves (left and right arrow keys)

--- GGJ-1-28-22/Assets/Scripts/RiftLine.cs
+++ GGJ-1-28-22/Assets/Scripts/RiftLine.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class RiftLine : MonoBehaviour
 {
+    LineRenderer theLine;
+     
     // Start is called before the first frame update
     void Start()
     {
+        theLine = this.GetComponent<LineRenderer>();
+        theLine.material = new Material(Shader.Find("Sprites/Default"));
+        theLine.SetColors(Color.cyan, Color.cyan);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 startPoint = theLine.GetPosition(0);
+        Vector3 endPoint= theLine.GetPosition(1);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            theLine.SetPosition(0, new Vector3(startPoint.x, startPoint.y-1, startPoint.z));
+            theLine.SetPosition(1, new Vector3(endPoint.x, endPoint.y + 1, endPoint.z));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            theLine.SetPosition(0, new Vector3(startPoint.x, startPoint.y + 1, startPoint.z));
+            theLine.SetPosition(1, new Vector3(endPoint.x, endPoint.y - 1, endPoint.z));
+        }
     }
 }
