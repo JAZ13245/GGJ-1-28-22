3f2bb3cef0b30b73effe49d86870371919657e47
ChloeEvans3363 <cre3363@g.rit.edu>
2022-01-30 14:05:53 -0500

    Prefab for spawning

--- GGJ-1-28-22/Assets/Scripts/SpawnBlock.cs
+++ GGJ-1-28-22/Assets/Scripts/SpawnBlock.cs
@@ -10,7 +10,7 @@ public class SpawnBlock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //yes
     }
 
     // Update is called once per frame
