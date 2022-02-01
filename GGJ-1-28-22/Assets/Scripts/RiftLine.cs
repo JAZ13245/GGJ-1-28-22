1335ddcf29188ddf1b073517489c9673fc09a946
Adrotic <adroticofficial@gmail.com>
2022-02-01 18:37:15 -0500

    cleaned up riftline class

--- GGJ-1-28-22/Assets/Scripts/RiftLine.cs
+++ GGJ-1-28-22/Assets/Scripts/RiftLine.cs
@@ -8,7 +8,6 @@ public class RiftLine : MonoBehaviour
     public GameObject RiftMask;
     public LevelGenerator lg;
     public float angle;
-    //public int quadrant;
 
     public BoxCollider2D lineCollision;
     public GameObject lineParticles;
@@ -25,11 +24,9 @@ public class RiftLine : MonoBehaviour
     public int playerTwoQuadrant;
 
 
-
     // Start is called before the first frame update
     void Start()
     {
-        //quadrant = 0;
         angle = 270;
         leftHeld = false;
         rightHeld = false;
@@ -41,8 +38,8 @@ public class RiftLine : MonoBehaviour
 
         theLine.SetPosition(1, new Vector3(0,0,-9));
         theLine.SetPosition(2, new Vector3(6, 0, -9));
-
     }
+
 
     // Update is called once per frame
     void Update()
@@ -60,103 +57,58 @@ public class RiftLine : MonoBehaviour
         ShiftAngle(GetLineQuad());
         SetAngle();
 
-        if(angle < 0){ angle += 360; }
+        if (angle < 0){ angle += 360; }
         if (angle >= 360 || angle <= -360) { angle = 0; }
     }
 
+
+    // returns the angle the rift line should be based on the quadrants of the two players
     private int GetLineQuad()
     {
-        if (playerOneQuadrant == 1)
-        {
-            if (playerTwoQuadrant == 2)
-            {
-                return 270;
-            }
-            if (playerTwoQuadrant == 3)
-            {
-                return 315;
-            }
-            if (playerTwoQuadrant == 4)
-            {
-                return 0;
-            }
+        if (playerOneQuadrant == 1) {
+            if (playerTwoQuadrant == 2) { return 270; }
+            if (playerTwoQuadrant == 3) { return 315; }
+            if (playerTwoQuadrant == 4) { return 0; }
         }
-        else if (playerOneQuadrant == 2)
-        {
-            if (playerTwoQuadrant == 1)
-            {
-                return 90;
-            }
-            if (playerTwoQuadrant == 3)
-            {
-                return 0;
-            }
-            if (playerTwoQuadrant == 4)
-            {
-                return 45;
-            }
+        else if (playerOneQuadrant == 2) {
+            if (playerTwoQuadrant == 1) { return 90; }
+            if (playerTwoQuadrant == 3) { return 0; }
+            if (playerTwoQuadrant == 4) { return 45; }
         }
-        else if (playerOneQuadrant == 3)
-        {
-            if (playerTwoQuadrant == 1)
-            {
-                return 135;
-            }
-            if (playerTwoQuadrant == 2)
-            {
-                return 180;
-            }
-            if (playerTwoQuadrant == 4)
-            {
-                return 90;
-            }
+        else if (playerOneQuadrant == 3) {
+            if (playerTwoQuadrant == 1) { return 135; }
+            if (playerTwoQuadrant == 2) { return 180; }
+            if (playerTwoQuadrant == 4) { return 90; }
         }
-        else if (playerOneQuadrant == 4)
-        {
-            if (playerTwoQuadrant == 1)
-            {
-                return 180;
-            }
-            if (playerTwoQuadrant == 2)
-            {
-                return 225;
-            }
-            if (playerTwoQuadrant == 3)
-            {
-                return 270;
-            }
+        else if (playerOneQuadrant == 4) {
+            if (playerTwoQuadrant == 1) { return 180; }
+            if (playerTwoQuadrant == 2) { return 225; }
+            if (playerTwoQuadrant == 3) { return 270; }
         }
         return -1;
     }
 
-    private void ShiftAngle(int desiredAngle)
-    {
-        if (desiredAngle != angle)
-        {
 
+    // Adds feathering to the angle changing
+    // Instead of instantly going to the angle desired, transition to it overtime
+    private void ShiftAngle(int desiredAngle) {
+        if (desiredAngle != angle) {
             if(desiredAngle == 0) {
                 if(angle > 180) { angle += 5; }
                 if(angle < 180) { angle -= 5; }
             }
-            else if(angle == 0)
-            {
+            else if(angle == 0) {
                 if(desiredAngle > 180) { angle -= 5; }
                 if(desiredAngle < 180) { angle += 5; }
             }
-            else if (desiredAngle > angle) {
-                angle += 5;
-            }
-            else if (desiredAngle < angle) {
-                angle -= 5;
-            }
+            else if (desiredAngle > angle) { angle += 5; }
+            else if (desiredAngle < angle) { angle -= 5; }
         }
     }
-        
-    
 
 
-    private void SetAngle()
-    {
+    // set the rift line and mask to the current angle
+    private void SetAngle() {
         RiftMask.transform.rotation = Quaternion.identity;
         RiftMask.transform.Rotate(new Vector3(0, 0, angle));
 
@@ -173,8 +125,9 @@ public class RiftLine : MonoBehaviour
         angle = newAngle;
     }
 
-    private int GetQuadrant(Vector3 obj)
-    {
+
+    // takes in a player's vector3 and returns what qudrant they're in
+    private int GetQuadrant(Vector3 obj) {
         if (obj.x > 0 && obj.y > 0) { return 1; }
         else if (obj.x < 0 && obj.y > 0) { return 2; }
         else if (obj.x < 0 && obj.y < 0) { return 3; }
@@ -182,83 +135,3 @@ public class RiftLine : MonoBehaviour
         else return 0;
     }
 }
-
-
-
-
-/*
-        gm.GetComponent<LineRenderer>().SetPosition(1, (playerOnePos + (playerTwoPos - playerOnePos) / 2));
-        /*
-        gm.GetComponent<LineRenderer>().SetPosition(1, new Vector3(Mathf.Abs(gm.GetComponent<LineRenderer>().GetPosition(1).x),
-             Mathf.Abs(gm.GetComponent<LineRenderer>().GetPosition(1).y),-9));
-        
-        gm.GetComponent<LineRenderer>().SetPosition(0, new Vector3(gm.GetComponent<LineRenderer>().GetPosition(1).x, 0, -9));
-        gm.GetComponent<LineRenderer>().SetPosition(2, new Vector3(0, 0, -9));
-        gm.GetComponent<LineRenderer>().SetPosition(3, (playerOnePos + (playerTwoPos - playerOnePos) / 2));
-        gm.GetComponent<LineRenderer>().SetPosition(4, new Vector3(0, gm.GetComponent<LineRenderer>().GetPosition(1).y, -9));
-
-
-        Vector3 v0 = gm.GetComponent<LineRenderer>().GetPosition(0);
-        Vector3 v1 = gm.GetComponent<LineRenderer>().GetPosition(1);
-        Vector3 v2 = gm.GetComponent<LineRenderer>().GetPosition(2);
-        Vector3 v3 = gm.GetComponent<LineRenderer>().GetPosition(3);
-        Vector3 v4 = gm.GetComponent<LineRenderer>().GetPosition(4);
-
-
-        float l1 = (v0 - v1).magnitude;
-        float l2 = (v1 - v2).magnitude;
-        float l3 = (v2 - v3).magnitude;
-        float l4 = (v3 - v4).magnitude;
-        
-        if(v1.x > 0 && v1.y > 0) { quadrant = 1; }
-        if(v1.x < 0 && v1.y > 0) { quadrant = 2; }
-        if(v1.x < 0 && v1.y < 0) { quadrant = 3; }
-        if(v1.x > 0 && v1.y < 0) { quadrant = 4; }
-        
-        int rightPlayer;
-        int topPlayer;
-
-
-        if (playerTwoPos.y > playerOnePos.y)
-        {
-            topPlayer = 2;
-        }
-        else
-        {
-            topPlayer = 1;
-        }
-        if (playerTwoPos.x > playerOnePos.x)
-        {
-            rightPlayer = 2;
-        }
-        else
-        {
-            rightPlayer = 1;
-        }
-
-        //if (quadrant == 1 && topPlayer == 2) { quadrant = 3; }
-        if (quadrant == 2 && topPlayer == 1) { quadrant = 4; }
-        else if (quadrant == 3 && topPlayer == 1) { quadrant = 1; }
-        else if (quadrant == 4 && topPlayer == 2) { quadrant = 2; }
-        
-
-        if (quadrant == 1)
-        {
-            angle = Mathf.Asin(l1 / l2) * Mathf.Rad2Deg;
-        }
-        if (quadrant == 2)
-        {
-            angle = Mathf.Asin(l4 / l3) * Mathf.Rad2Deg;
-            angle += 90;
-        }
-        else if (quadrant == 3)
-        {
-            angle = Mathf.Asin(l1 / l2) * Mathf.Rad2Deg;
-            angle += 180;
-        }
-        else if (quadrant == 4)
-        {
-            angle = Mathf.Asin(l4 / l3) * Mathf.Rad2Deg;
-            angle += 270;
-        }
-        */
