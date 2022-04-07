07323bb12f1acd34704aa638c563c7b2d33126df
Gamertag <cre3363@g.rit.edu>
2022-04-06 20:41:45 -0400

    merge

--- GGJ-1-28-22/Assets/Scripts/GameManager.cs
+++ GGJ-1-28-22/Assets/Scripts/GameManager.cs
@@ -6,14 +6,6 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    //public GameObject playerOnePrefab;
-    //public GameObject playerTwoPrefab;
-    /*
-    public PlayerInput playerOne;
-    public PlayerInput playerTwo;
-
-    public SpawnBlock spawnBlock;
-    */
     public GameObject loader;
     public GameObject generator;
 
@@ -28,18 +20,6 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Time.timeScale = 1;
-        //playerOne = generator.GetComponent<LevelGenerator>().playerOnePrefab;
-        //playerOne = GameObject.FindWithTag("PlayerOne");
-        //playerTwo = GameObject.FindWithTag("PlayerTwo");
-        //playerTwo = generator.GetComponent<LevelGenerator>().playerOnePrefab;
-        //playerTwo = generator.GetComponent<LevelGenerator>().playerTwoPrefab;
-
-        //playerOnePrefab = transform.Find("PlayerOne(Clone)").gameObject;
-        //playerTwoPrefab = transform.Find("PlayerTwo(Clone)").gameObject;
-        /*
-        playerOne = PlayerInput.Instantiate(playerOnePrefab, controlScheme: "PlayerOne", pairWithDevice: Keyboard.current);
-        playerTwo = PlayerInput.Instantiate(playerTwoPrefab, controlScheme: "PlayerTwo", pairWithDevice: Keyboard.current);
-        */
     }
 
     private void Update()
@@ -78,29 +58,6 @@ public class GameManager : MonoBehaviour
             }
         }       
 
-
-        /*if(Mathf.Abs(playerOnePrefab.GetComponent<Player>().position.x) > 300 
-            && Mathf.Abs(playerTwoPrefab.GetComponent<Player>().position.x) > 300)
-        {
-            loader.GetComponent<LevelLoader>().LoadNextLevel();
-        }
-
-        if (Mathf.Abs(playerOnePrefab.GetComponent<Player>().position.x) > 300
-            && Mathf.Abs(playerTwoPrefab.GetComponent<Player>().position.y) > 400)
-        {
-            loader.GetComponent<LevelLoader>().LoadNextLevel();
-        }
-
-        if (Mathf.Abs(playerOnePrefab.GetComponent<Player>().position.y) > 400
-            && Mathf.Abs(playerTwoPrefab.GetComponent<Player>().position.x) > 300)
-        {
-            loader.GetComponent<LevelLoader>().LoadNextLevel();
-        }
-
-        if (Mathf.Abs(playerOnePrefab.GetComponent<Player>().position.y) > 400
-            && Mathf.Abs(playerTwoPrefab.GetComponent<Player>().position.y) > 400)
-        {
-            loader.GetComponent<LevelLoader>().LoadNextLevel();
-        }*/
     }
+
 }
